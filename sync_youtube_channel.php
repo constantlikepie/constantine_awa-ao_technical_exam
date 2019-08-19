024d9cd8e797eacf6448839bacf9da514983df51
constantlikepie <52833361+constantlikepie@users.noreply.github.com>
2019-08-19 11:09:35 +0800

    Fixed

--- sync_youtube_channel.php
+++ sync_youtube_channel.php
@@ -10,18 +10,19 @@
 	$YT_CHANNEL_ID = $_GET['channel_id'];
 	$SYNC_DATA_TOKEN = "constantineawa-ao";
 	$YT_API_KEY = 'AIzaSyA5eKz-2GN19ZJ2Swa4nKq_zfzbIN34VnA';
-	// used 50 since that's the allowed for maximum result, tried 100 but gave it gave me a warning
+
 	$videos_count_to_read = 50;
 	$YT_WATCH_LINK = 'https://www.youtube.com/watch?v=';
 
 	$lemmesync = isset($_GET['token']) && $_GET['token'] == $SYNC_DATA_TOKEN;
 
 	$channel_id;
+	$yt_channel_id;
 
 	if($lemmesync)
 	{
 		$channel_data = json_decode(@file_get_contents('https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername='.$YT_CHANNEL_ID.'&key='.$YT_API_KEY));
-		
+		$yt_channel_id = $channel_data->items[0]->id;
 		if(is_null($channel_data))
 			die("Invalid channel ID or token given.");
 
@@ -50,38 +51,45 @@
 		}catch(PDOException $e){
 			die($e);
 		}
-
-		$videos = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&forUsername='.$YT_CHANNEL_ID.'&maxResults='.$videos_count_to_read.'&key='.$YT_API_KEY));
-
-		$videos = $videos->items;
-
-
-		foreach ($videos as $key => $value)
+		$read_count = 0;
+		$next_page_token = '';
+		while($read_count<100)
 		{
-			$video_title = $value->snippet->title;
-			$video_thumbnail = $value->snippet->thumbnails->default->url;
-			$video_url = $YT_WATCH_LINK.$value->id->videoId;
+			//if()
+			//	$next_page_token_inner = '&pageToken='.$next_page_token;
+			$videos = json_decode(file_get_contents('https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$yt_channel_id.'&maxResults='.$videos_count_to_read.'&key='.$YT_API_KEY.$next_page_token));
+			$next_page_token = '&pageToken='.$videos->nextPageToken;
 
-			$query = 'INSERT INTO youtube_channel_videos (title, thumbnail, video_url, youtube_channel_id) VALUES (:video_title, :video_thumbnail, :video_url, :channel_id)';
+			$videos = $videos->items;
 
-			try{
-				$db = new DB();
-				$db = $db->connect();
+			foreach ($videos as $key => $value)
+			{
+				$video_title = $value->snippet->title;
+				$video_thumbnail = $value->snippet->thumbnails->default->url;
+				$video_url = $YT_WATCH_LINK.$value->id->videoId;
 
-				$statement = $db->prepare($query);
+				$query = 'INSERT INTO youtube_channel_videos (title, thumbnail, video_url, youtube_channel_id) VALUES (:video_title, :video_thumbnail, :video_url, :channel_id)';
 
-				$statement->bindParam(':video_title', $video_title);
-				$statement->bindParam(':video_thumbnail', $video_thumbnail);
-				$statement->bindParam(':video_url', $video_url);
-				$statement->bindParam(':channel_id', $channel_id);
+				try{
+					$db = new DB();
+					$db = $db->connect();
 
-				$statement->execute();
+					$statement = $db->prepare($query);
 
-						$db = null;
+					$statement->bindParam(':video_title', $video_title);
+					$statement->bindParam(':video_thumbnail', $video_thumbnail);
+					$statement->bindParam(':video_url', $video_url);
+					$statement->bindParam(':channel_id', $channel_id);
 
-					}catch(PDOException $e){
-						die($e);
-					}
+					$statement->execute();
+
+							$db = null;
+
+						}catch(PDOException $e){
+							die($e);
+						}
+			}
+			$read_count = $read_count+50;
 		}
 		header("Location: http://localhost/constantine_awa-ao_technical_exam/show_youtube_channel.html?channel_id=".$channel_id);
 	}
